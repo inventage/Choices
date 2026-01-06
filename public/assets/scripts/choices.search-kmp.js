d1a18d8878cbdd8554e8a034e119e453ce68b2f1
Niklaus Hug <niklaus.hug@inventage.com>
2026-01-06 12:26:55 +0100

    Disable Choices instance if attribute readonly is set.
    - Attribute readonly triggers same behaviour like attribute disabled does.

--- public/assets/scripts/choices.search-kmp.js
+++ public/assets/scripts/choices.search-kmp.js
@@ -1924,6 +1924,7 @@
             this._createElements();
             this._createStructure();
             if ((this._isTextElement && !this.config.addItems) ||
+                this.passedElement.element.hasAttribute('readonly') ||
                 this.passedElement.element.hasAttribute('disabled') ||
                 !!this.passedElement.element.closest('fieldset:disabled')) {
                 this.disable();
